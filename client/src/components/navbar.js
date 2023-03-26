245129a90bd8c493b1c1dbef78eb5afb056ba932
James Trombo <jamestrombo@gmail.com>
2023-03-26 10:54:09 -0400

    change name to be accurate

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -5,7 +5,7 @@ export default function NavBar(props){
 return (
     <div className="navbar bg-base-100 fixed">
   <div className="navbar-start">
-    <a className="btn btn-ghost normal-case text-xl">Quick Exit</a>
+    <a className="btn btn-ghost normal-case text-xl">Quick Escape</a>
   </div>
   <div className="navbar-end">
     <div className="w-10 rounded-full">
